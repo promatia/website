f19cd8ed3908fb46dddfb32de1b6e7c5ca872939
DominusVilicus <albertmashy@outlook.com>
2019-11-06 14:39:05 +1030

    Fixed www redirect issue

--- server/routes/middleware/noWWW.js
+++ server/routes/middleware/noWWW.js
@@ -1,6 +1,8 @@
 module.exports = async (ctx, next) => {
     if(/^www\./.test(ctx.host)){
-        return ctx.redirect(ctx.protocol + ctx.host.replace(/^www\./, '') + ctx.url)
+        ctx.status = 301
+        
+        return ctx.redirect(`${ctx.protocol}://${ctx.host.replace(/^www\./, '')}${ctx.url}`)
     }
     await next()
 }
