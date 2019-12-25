9dd58f0d31574ed21c1c4c49b5db1e8807de7492
DominusVilicus <albertmashy@outlook.com>
2019-12-26 00:00:22 +1030

    Fix bugs

--- resources/pages/start.vue
+++ resources/pages/start.vue
@@ -172,7 +172,7 @@ export default {
         ]
 
         onMounted(()=>{
-            setCookie('referrer', getQuery('referrer'))
+            if(getQuery('referrer')) setCookie('referrer', getQuery('referrer'))
         })
 
         return {
