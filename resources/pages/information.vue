ff6ffb217241e8f226a6e69ad1ae980eab31c859
SoaringMoon <sori.phone@gmail.com>
2020-01-11 06:29:32 -0500

    information pages update
    adds information page, media page, values page, and changes error page.

---
+++ resources/pages/information.vue
@@ -0,0 +1,131 @@
+<template>
+    <web>
+        <template slot="title">Information</template>
+        <template slot="content">
+            <div class="wrapper">
+                <h1 class="big-text">
+                    Information Hub
+                </h1>
+                <div class="grid-list">
+                    <div>
+                        <a href="/information/values" class="fill-div">Vision &amp; Values
+                        <p>Promatia has a detailed vision of the future. Find out how you can help us help you.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/government" class="fill-div">Our Government
+                        <p>Learn more about Promatia's unique system of government, how laws are passed, and how Ministires are formed and run.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/plan" class="fill-div">Our Plan
+                        <p>See how you plan to bring Promatia in to fruition. Get a roadmap of events and updates.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/citizenship" class="fill-div">Citizenship
+                        <p>Find out what being a citizen of Promatia will mean to you, and how to becoming a citizen of Promatia can help the project.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/mypro" class="fill-div">myPro
+                        <p>Need to know more about myPro and how to best utilize the resources it provides? We have you covered with this indepth guide about the myPro platform.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/services" class="fill-div">Services
+                        <p>It can be a little confusing trying to find a specific service we offer. So we put them all into one easy to access location.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/lumina" class="fill-div">Lumina
+                        <p>Lumina will be Promatia's first city. Learn about how it will be built, what techonologies we will use, and what to look forward to.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/voting" class="fill-div">Voting
+                        <p>Need help getting involved in Promatia's direct democracy? No problem! You can inform yourself on the voting process here.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/business" class="fill-div">Starting a Business
+                        <p>Do you plan to start or operate an existing business in Promatia? We can help you set up your business to get it up and running smoothly.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/proma" class="fill-div">Monetary System
+                        <p>Learn more about Promatia's unique digital currency, the Proma, and how using it can automate taxes and transactions with PromaBank.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/education" class="fill-div">Education
+                        <p>Promatia aims to provide world class education curriculum. Learn how to register and reroll in our education programs.</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/information/work" class="fill-div">Work &amp; Employment
+                        <p>Applying for a new job? Promatia lists every employer looking to hire. Find out how to use this tool to job search.</p>
+                        </a>
+                    </div>
+                </div>
+                <p>
+                    We also offer a wide range of <a href="/information/services">Government Services</a>.
+                </p>
+            </div>
+        </template>
+    </web>
+</template>
+<style lang="stylus" scoped>
+a p
+    color #404040
+    font-weight 400
+.subtitle
+    font-weight 500
+    font-size 1.2em
+.big-text
+    margin 0
+
+.wrapper
+    padding 80px 40px
+    max-width: 1000px
+    margin: auto
+
+.grid-list
+    display grid
+    grid-gap 0 0px
+    > * 
+        border-radius 5px
+        border-left 5px solid #0080ff
+        margin 10px
+        background-color #ffffff
+        height auto
+        padding 4px
+    > * p
+        margin-bottom 0px
+    > * p:hover
+        margin-bottom 0px
+    > *:hover
+        border 2px solid #0080ff
+        border-left 5px solid #0080ff
+        background-color #f8f8f8
+        padding 2px
+        padding-left 4px
+    grid-template-columns repeat(auto-fit, minmax(300px, 1fr))
+    a.fill-div
+        display block
+        height 100%
+        width 100%
+        text-decoration: none
+</style>
+<script>
+import web from "@/layouts/web"
+
+export default {
+    async serverPrefetch(){
+        this.$ssrContext.ctx.status = 404
+    },
+    components: {
+        web
+    }
+}
+</script>
---
+++ resources/pages/information/values.vue
@@ -0,0 +1,65 @@
+<template>
+    <web>
+        <template slot="title">Values</template>
+        <template slot="content">
+            <div class="wrapper">
+                <h1 class="big-text">
+                    Promatian Values
+                </h1>
+                <p>
+                    Promatia is a new country project located in the continent of Australia, bordering the coast of the Gulf of Carpentaria. The project was first invisioned in November of 1999 as has maintained stable workflow since. After 20 years of hard work, we have pulled back the curtains on the project in order to promote a more public image.
+                </p>
+                <p>
+                    Promatia is a project that aims to construct a peaceful and co-operative self-governing body a the most remote part of the Australian continent through building of scalable infrastructure. It is a collection of bright minds from a new generation with the expressed goal of establishing a technologically innovative, and environmentally sustainable platform for transparent and direct democracy. Promatia provides open and democratic architecture of digital governance. A place where any citizen can contribute to the rule of law.
+                </p>
+                <p>
+                    Our project aims to create a national identity and place of belonging for intelligent, innovative, and inquisitive minds. Giving people a chance to express their creativity and educate themselves. It strives to become a community of simplicity. From a simple majority voting structure, to expectable rates in the application of our tax system, to clear and concise legal language.
+                </p>
+                <p>
+                    It is important to Promatia that its people feel free from the threat of corruption. Promatia has been founded by a like mineded group of people wary of the condition of many of the world's governments. We see them as influenced by corporate interests and extra-national interference. Promatia wishes to become a place where people have the power to change their own policy, and make and enforce that policy in real time.
+                </p>
+                <p>
+                    Promatia is not a hoax, not a prank, and not a teenage dream. We are a collaboration of like minded individuals from around the globe with knowledge in diverse fields ranging from information security and public key cryptography, architectural design and city planing, and machine learning.
+                </p>
+                <p>
+                <h1 class="big-text">
+                    Skills
+                </h1>
+                <p>
+                    The Promatian government recruits as many interested people as possible to maintain a high baseline of manpower in order to make our its dream a reality. We look for specialists in their field, so that the can use their skills for the purpose of building a better world. Here is a short list of the kind skill Promatia recruits for, and why they part of creating and executing the Promatian vision.
+                </p>
+                <ul>
+                    <li><b>App Development:</b> Promatia has the experience to develop web applications accessible to every individual, even those outside of the nation, to conduct business transactions with its digital currency in a seamless fashion.</li><br>
+                    <li><b>Computer-Aided Design:</b> Our government employees have the skills to organize cities, buildings, infrastructure, inventions, as well as other designs using computer aided design and graphic design software. A breath of fresh air for people tired of archaic paper forms, and walls of black and white when taxes come around.</li><br>
+                    <li><b>Rapid Prototyping:</b> Promatia streamlines every part of its organizational hierarchy. Gone are the days of waiting years for changes to come. Expertise with powerful and premium tools in the design industry, allow our government to rapidly prototype new applications and features in the digital workspace for the progress of digital governance.</li><br>
+                    <li><b>Machine Learning:</b> We specialize in the development of powerful machine learning models, artificial intelligence, and deep neural networks at the bleeding edge of technology. Our research helps get to the core of what the citizenship really needs, and how the government can best deliver on its promises. Promatia seeks to automate as much of the governmental process as possible, to allow its employees to focus more closely on the problems of individuals.</li>
+                </ul>
+            </div>
+        </template>
+    </web>
+</template>
+<style lang="stylus" scoped>
+.subtitle
+    font-weight 500
+    font-size 1.2em
+.big-text
+    margin 0
+
+.wrapper
+    padding 80px 40px
+    max-width: 1000px
+    margin: auto
+
+</style>
+<script>
+import web from "@/layouts/web"
+
+export default {
+    async serverPrefetch(){
+        this.$ssrContext.ctx.status = 404
+    },
+    components: {
+        web
+    }
+}
+</script>
