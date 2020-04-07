9bb2692e7e4e5941ea53eec1cc1cb079adf364eb
DominusVilicus <albertmashy@outlook.com>
2020-04-07 16:01:43 +0930

    Add Icon CurrentColors

--- resources/components/icons/Laws.vue
+++ resources/components/icons/Laws.vue
@@ -1,6 +1,6 @@
 <template>
     <span class="material-design-icon">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path d="M17.8 20C17.4 21.2 16.3 22 15 22H5C3.3 22 2 20.7 2 19V18H5H14.2C14.6 19.2 15.7 20 17 20H17.8ZM19 2C20.7 2 22 3.3 22 5V6H20V5C20 4.4 19.6 4 19 4C18.4 4 18 4.4 18 5V18H17C16.4 18 16 17.6 16 17V16H5V5C5 3.3 6.3 2 8 2H19ZM8 6V8H15V6H8ZM8 10V12H14V10H8Z"/>
         </svg>
     </span>
